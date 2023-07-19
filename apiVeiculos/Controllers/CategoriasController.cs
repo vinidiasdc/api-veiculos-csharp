97680477b44e87452e93f0544fc29d8f86797b6f
Vinicius Dias <vinidiasti21@gmail.com>
2023-07-18 22:21:34 -0300

    adicionando endpoints(edit/add/getid)

--- apiVeiculos/Controllers/CategoriasController.cs
+++ apiVeiculos/Controllers/CategoriasController.cs
@@ -1,6 +1,5 @@
 ﻿using apiVeiculos.DTOs;
 using apiVeiculos.Entidades;
-using apiVeiculos.Repositorios;
 using apiVeiculos.Repositorios.Interfaces;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
@@ -33,6 +32,48 @@ namespace apiVeiculos.Controllers
             IEnumerable<CategoriaDTO> categoriasDTO = _mapper.Map<IEnumerable<CategoriaDTO>>(categorias);
 
             return Ok(categoriasDTO);
+        }
+
+        [HttpGet("{id:int}", Name = "ConsultarCategoria")]
+        public async Task<ActionResult<CategoriaDTO>> ConsultarId(int id) {
+            Categoria categoria = await _categoriasRepository.ConsultePorIdAsync(id);
+
+            if (categoria is null)
+            {
+                return NotFound("Categorias não encontradas!");
+            }
+
+            CategoriaDTO categoriaDTO = _mapper.Map<CategoriaDTO>(categoria);
+
+            return Ok(categoriaDTO);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Adicionar([FromBody] CategoriaDTO categoriaDTO) {
+            if(categoriaDTO is null) {
+                return BadRequest("Dados inválidos!");
+            }
+
+            Categoria categoria = _mapper.Map<Categoria>(categoriaDTO);
+
+            await _categoriasRepository.AddAsync(categoria);
+
+            return new CreatedAtRouteResult("ConsultarCategoria", new { id = categoriaDTO.Id }, categoriaDTO);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> Alterar(int id, [FromBody] CategoriaDTO categoriaDTO) {
+            if(id != categoriaDTO.Id)
+                return BadRequest();
+
+            if(categoriaDTO is null)
+                return BadRequest();
+
+            var categoria = _mapper.Map<Categoria>(categoriaDTO);
+
+            await _categoriasRepository.AtualizeAsync(categoria);
+
+            return Ok(categoriaDTO); 
         }
     }
 }
